45de55a39a9422ba6bd82c51308fadadb3ea0b39
Abenet <abebekezhabesha@gmail.com>
2023-06-15 13:53:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
  * Return: 1 (Success)
  */
-int main(void)
-{
-	fput("and that piece of art is useful\" - Dora Ko
-			rpar, 2015-10-19\n", stdout);
-	return (1);
+
+int main (void)
+{ 
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+        return (1);
 }
