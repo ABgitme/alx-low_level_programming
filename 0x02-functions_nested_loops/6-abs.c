c68b67d57ecd60596467b17b52d208964a3394a5
Abenet <abebekezhabesha@gmail.com>
2023-06-20 14:28:00 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - that prints signs
+ * @n: value to be checked
+ *
+ * Return: 1 if n is greater than zero. 0 if n is zero.-1 n is less than zero
+ */
+int _abs(int n)
+{
+return (abs(n));
+}
