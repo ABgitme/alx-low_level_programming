db43cfc0922de44d134712b7f93a59171c0e8a22
Abenet <abebekezhabesha@gmail.com>
2023-06-23 08:08:14 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit- check the code.
+ * @c: value check
+ * Return: 1 if c is uppercase
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+return (0);
+}
