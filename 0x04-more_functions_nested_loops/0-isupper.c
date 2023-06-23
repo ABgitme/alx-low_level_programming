ee938ac1be3a8985aa531e7c0719e294b3452deb
Abenet <abebekezhabesha@gmail.com>
2023-06-23 07:59:11 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper- check the code.
+ * @c: value check
+ * Return: 1 if c is uppercase
+ */
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+return (0);
+}
