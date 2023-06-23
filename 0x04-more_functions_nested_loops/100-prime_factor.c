39f8dc8e6f727caacf68dd318e6b57be970241a1
Abenet <abebekezhabesha@gmail.com>
2023-06-23 14:24:52 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+unsigned long int i = 3, num = 612852475143;
+for (; i < 12057; i += 2)
+{
+while (num % i == 0 && num != i)
+num /= i;
+}
+printf("%lu\n", num);
+return (0);
+}
