3295d53f1370c22680471e6606da868807889089
Abenet <abebekezhabesha@gmail.com>
2023-06-30 00:40:44 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,18 +10,16 @@ int _strcmp(char *s1, char *s2)
 int i;
 for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
-if (s1[i] == s2[i])
+if (s1[i] > s2[i])
 {
-return (0);
+return (s1[i] - s2[i]);
 break;
 }
 else if (s1[i] < s2[i])
 {
-return (-15);
+return (s1[i] - s2[i]);
 break;
 }
-else
-break;
 }
-return (15);
+return (0);
 }
