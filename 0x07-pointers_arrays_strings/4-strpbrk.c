afe2a9dac5a39443c947344ffb74a309c0ba6f31
Abenet <abebekezhabesha@gmail.com>
2023-07-04 07:19:15 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,23 +8,34 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-char *tmp;	
-if (s == NULL || accept == NULL)
+int i;
+int j;
+int pos;
+int flag = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+pos = i;
+}
+for (i = 0; accept[i] != '\0'; i++)
+{
+for (j = 0; s[j] != '\0'; j++)
+{
+if (accept[i] == s[j])
+{
+if (j <= pos)
+{
+pos = j;
+flag = 1;
+}
+}
+}
+}
+if (flag == 1)
+{
+return (&s[pos]);
+}
+else
 {
 return (NULL);
 }
-while (*s != '\0')
-{
-tmp = accept;
-while (*tmp != '\0')
-{
-if (*s == *tmp)
-{
-return (s);
-}
-tmp++;
-}
-s++;
-}
-return (NULL);
 }
