a54a3ec8dcdf371b485af4b7bcbc01e725cb390d
Abenet <abebekezhabesha@gmail.com>
2023-07-05 07:37:55 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#incude "main.h"
+/**
+ * _print_rev_recursion - put function recursion
+ * @s: the string
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
