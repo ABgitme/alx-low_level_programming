64600d966599eaa5a47e495c1f6b1ece25ebb9e3
ABgitme <abebekezhabesha@gmail.com>
2023-07-26 22:41:22 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,21 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
+#include <stddef.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - malloc string value
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 char _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
