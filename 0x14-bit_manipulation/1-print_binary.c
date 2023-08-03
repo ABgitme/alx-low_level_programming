41022c07085755bfc540e0eea9eaf3a1536ba005
ABgitme <abebekezhabesha@gmail.com>
2023-08-03 11:13:15 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,35 +1,14 @@
 #include "main.h"
 
 /**
- * _log2 - function to convert int to base 2
- * @x : int value to convert
- * return: base 2 value
- */
-unsigned int _log2(unsigned int x) {
-  unsigned int val = 0;
-
-  while (x > 0) {
-    val++;
-    x >>= 1;
-  }
-
-  return val;
-}
-
-/**
  * print_binary - function to convert int to binary
- * @n: int value to convert
+ * @n : int value to convert
  */
 void print_binary(unsigned long int n) {
-  int len;
-
-  len = _log2(n) - 1;
-  if (n == 0) {
-    _putchar('0');
-    return;
-  }
-
-  for (; len >= 0; len--) {
-    _putchar('0' + ((n >> len) & 1));
-  }
+    if (n == 0) {
+        _putchar('0');
+	return;
+    }
+    print_binary(n >> 1); 
+    _putchar(n & 1 ? '1' : '0');
 }
