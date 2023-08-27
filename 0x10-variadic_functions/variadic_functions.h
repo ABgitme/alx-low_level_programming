6c45dede79fa34f6293753b0ad3a5069e2b230d9
ABgitme <132369839+ABgitme@users.noreply.github.com>
2023-08-27 12:23:53 +0300

    Delete 0x10-variadic_functions directory

--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef VARDIATIC_FUNCTIONS_H
-#define VARDIATIC_FUNCTIONS_H
-char _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-#endif
