1a45fbf888786ee6b9a620239435601b13c07dd0
ABgitme <abebekezhabesha@gmail.com>
2023-09-04 08:45:41 +0300

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,57 +13,17 @@ char *str_concat(char *s1, char *s2)
 	char *new_str;
 	int len1 = strlen(s1);
 	int len2 = strlen(s2);
-	
-	new_str = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (new_str == NULL)
-		return NULL;
-	_memcpy(new_str, s1, len1);
-	_memcpy(new_str + len1, s2, len2 + 1);
-	return new_str;
-/*	char *new_str;
-	char *starts1;
-	char *starts2;
-	int i = 0;
-	int lens1 = 0;
-	int lens2 = 0;
 
-	starts1 = s1;
-	starts2 = s2;
 	if (s1 == NULL)
 		s1 = "";
-	while (*s1)
-	{
-		lens1++;
-		s1++;
-	}
-	s1 = starts1;
 	if (s2 == NULL)
 		s2 = "";
-	while (*s2)
-	{
-		lens2++;
-		s2++;
-	}
-	s2 = starts2;
-	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
-	starts1 = new_str;
+	new_str = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (new_str == NULL)
 		return (NULL);
-	for (; i < (lens1 + lens2); i++)
-	{
-		if (i < lens1)
-		{
-			new_str[i] = *s1;
-			s1++;
-		}
-		else
-		{
-			new_str[i] = *s2;
-			s2++;
-		}
-	}
-	new_str[i] = '\0';
-	return (starts1);*/
+	_memcpy(new_str, s1, len1);
+	_memcpy(new_str + len1, s2, len2 + 1);
+	return (new_str);
 }
 
 /**
@@ -78,7 +38,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	char *ch_dest = (char *) dest;
 	char *ch_src = (char *) src;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		ch_dest[i] = ch_src[i];
