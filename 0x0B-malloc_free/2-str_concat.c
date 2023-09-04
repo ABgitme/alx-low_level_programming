58032ab6a5e4c98533cad6ea4547dce4eb683c0d
ABgitme <abebekezhabesha@gmail.com>
2023-09-04 08:27:46 +0300

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,7 @@
 #include <stdlib.h>
+#include <string.h>
+
+char *_memcpy(char *dest, char *src, unsigned int n);
 /**
  * str_concat - A function that concatenates two strings
  * @s1: An input pointer of the first string
@@ -8,6 +11,16 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *new_str;
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+	
+	new_str = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (new_str == NULL)
+		return NULL;
+	_memcpy(new_str, s1, len1);
+	_memcpy(new_str + len1, s2, len2 + 1);
+	return new_str;
+/*	char *new_str;
 	char *starts1;
 	char *starts2;
 	int i = 0;
@@ -50,5 +63,25 @@ char *str_concat(char *s1, char *s2)
 		}
 	}
 	new_str[i] = '\0';
-	return (starts1);
+	return (starts1);*/
+}
+
+/**
+ * _memcpy - sets char
+ * @dest: char to set
+ * @src: char
+ * @n: number of bytes to print
+ * Return: char destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *ch_dest = (char *) dest;
+	char *ch_src = (char *) src;
+	
+	for (i = 0; i < n; i++)
+	{
+		ch_dest[i] = ch_src[i];
+	}
+	return (dest);
 }
