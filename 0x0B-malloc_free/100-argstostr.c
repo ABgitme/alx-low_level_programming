270338b0aedc67c4e1490217e4de236086406451
ABgitme <abebekezhabesha@gmail.com>
2023-09-04 07:36:04 +0300

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,52 +9,23 @@
  */
 char *argstostr(int ac, char **av)
 {
-int len = 0;
-int i;
-char *s;
-  for (i = 0; i < ac; i++) {
-    len += strlen(av[i]);
-  }
-  s = malloc(len + ac + 1); 
-  if (s == NULL) return NULL;
-
-  for (i = 0; i < ac; i++) {
-    strcat(s, av[i]);
-    strcat(s, "\n");
-    strcat(s, "\0");
-  }
-
-  return s;
-/*int ch = 0, i = 0, j = 0, k = 0;
-char *s;
-if (ac == 0 || av == NULL)
-return (NULL);
-while (i < ac)
-{
-while (av[i][j])
-{
-ch++;
-j++;
-}
-j = 0;
-i++;
-}
-s = malloc((sizeof(char) * ch) +(ac + 1));
-i = 0;
-while (av[i])
-{
-while (av[i][j])
-{
-s[k] = av[i][j];
-k++;
-j++;
-}
-s[k] = '\n';
-j = 0;
-k++;
-i++;
-}
-k++;
-s[k] = '\0';
-return (s);*/
+	int len = 0;
+	int i;
+	char *s;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		len += strlen(av[i]);
+	}
+	s = malloc(len + ac + 1);
+	if (s == NULL)
+		return NULL;
+	for (i = 0; i < ac; i++)
+	{
+		strcat(s, av[i]);
+		strcat(s, "\n");
+		strcat(s, "\0");
+	}
+	return (s);
 }
