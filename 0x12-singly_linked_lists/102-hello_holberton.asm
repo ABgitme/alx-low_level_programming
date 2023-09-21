bbf3f449eb72fa0c7a40f01f1805d42909804d27
ABgitme <abebekezhabesha@gmail.com>
2023-09-21 08:07:50 +0300

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/102-hello_holberton.asm
+++ 0x12-singly_linked_lists/102-hello_holberton.asm
@@ -1,19 +1,18 @@
 extern printf
 
-section .data
+	section .data
 msg:		db "Hello, Holberton", 0
 fmt:		db "%s", 10, 0
 
-section .text
+	section .text
 
-global main
+	global main
 main:
+	push	rbp
 	mov	rdi,fmt
 	mov	rsi,msg
 	mov	rax,0
 	call	printf
-	mov	rax,60
+	pop	rbp
+	mov	rax,0
 	ret
-
-
-
