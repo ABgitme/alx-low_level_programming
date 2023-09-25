9546bcb064c68a187f18d4e6eb3c399f338fcfce
ABgitme <abebekezhabesha@gmail.com>
2023-09-25 22:05:37 +0300

    betty update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *p;
 	listint_t *new_node = malloc(sizeof(listint_t));
-	
+
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
@@ -22,7 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	p = *head;
 	while (p->next != NULL)
-  		p = p->next;
+		p = p->next;
 	p->next = new_node;
 	return (new_node);
 }
