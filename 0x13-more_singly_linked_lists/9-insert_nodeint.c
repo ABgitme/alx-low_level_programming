fc83c0020fd4c0a6ea002a3872c6a1b232fac9bc
ABgitme <abebekezhabesha@gmail.com>
2023-09-25 22:44:00 +0300

    update task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,9 +20,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-			return (NULL);
-		new_node->n = n;
-		new_node->next = NULL;
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new_node;
