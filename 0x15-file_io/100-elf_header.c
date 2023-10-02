58244880cc71d2efa3f80b39ab79cf8a619c32b8
ABgitme <abebekezhabesha@gmail.com>
2023-10-02 09:28:30 +0300

    update elf100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,66 +2,78 @@
 #include <elf.h>
 #include <sys/mman.h>
 
-#define ELF_MAGIC "\177ELF\0"
-#define ELF32_MAGIC "\177ELF32\0"
-#define ELF64_MAGIC "\177ELF64\0"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-struct elf_header {
-    char magic[4];
-    uint16_t class;
-    uint16_t data;
-    uint32_t version;
-    uint8_t osabi;
-    uint8_t abiversion;
-    uint32_t type;
-    uintptr_t entrypoint;
+struct Elf64_Ehdr
+{
+    unsigned char e_ident[16];
+    Elf64_Off e_phoff;
+    Elf64_Off e_shoff;
+    Elf64_Word e_flags;
+    Elf64_Half e_ehsize;
+    Elf64_Half e_phentsize;
+    Elf64_Half e_phnum;
+    Elf64_Half e_shentsize;
+    Elf64_Half e_shnum;
+    Elf64_Half e_shstrndx;
 };
 
-static void print_error(const char *msg) {
-    fprintf(stderr, "%s\n", msg);
-    exit(98);
-}
-
-static int read_elf_header(FILE *file) {
-    struct elf_header header;
+void display_elf_header(const char *elf_filename)
+{
     size_t bytes_read;
-    bytes_read = fread(&header, sizeof(header), 1, file);
-    if (bytes_read != 1) {
-        print_error("Failed to read ELF header");
+    struct Elf64_Ehdr elf_header;
+    /*Open the ELF file.*/
+    FILE *fp = fopen(elf_filename, "rb");
+    if (fp == NULL)
+    {
+        printf("Error: Could not open file %s\n", elf_filename);
+        exit(98);
     }
-    return header.magic[0] == 'E' && header.magic[1] == 'L' && header.magic[2] == 'F';
+
+    /*Seek to the start of the ELF header.*/
+    if (lseek(fileno(fp), 0, SEEK_SET) < 0)
+    {
+        printf("Error: Could not seek to the start of the ELF header\n");
+        exit(98);
+    }
+
+    /*Read the ELF header.*/
+
+    bytes_read = fread(&elf_header, sizeof(elf_header), 1, fp);
+    if (bytes_read != 1)
+    {
+        printf("Error: Could not read the ELF header\n");
+        exit(98);
+    }
+
+    /*Display the ELF header information.*/
+    printf("Magic: %02x %02x %02x %02x %02x %02x %02x %02x\n",
+           elf_header.e_ident[0], elf_header.e_ident[1], elf_header.e_ident[2],
+           elf_header.e_ident[3], elf_header.e_ident[4], elf_header.e_ident[5],
+           elf_header.e_ident[6], elf_header.e_ident[7]);
+    printf("Class: %c\n", elf_header.e_ident[EI_CLASS]);
+    printf("Data: %c\n", elf_header.e_ident[EI_DATA]);
+    printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
+    printf("OS/ABI: %d\n", elf_header.e_ident[EI_OSABI]);
+    printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
+    printf("Type: %d\n", elf_header.e_ident[EI_PAD]);
+    printf("Entry point address: 0x%lx\n", elf_header.e_ident[EI_NIDENT]);
+
+    /*Close the ELF file.*/
+    fclose(fp);
 }
 
-int main(int argc, char **argv) {
-    	struct elf_header header;
-    	size_t bytes_read;
-	FILE *file;
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: elf_header elf_filename\n");
+        exit(98);
+    }
 
-	if (argc != 2) {
-        print_error("Usage: elf_header elf_filename");
-    }
-    file = fopen(argv[1], "rb");
-    if (!file) {
-        print_error("Failed to open file");
-    }
-    /* Check for ELF Magic number*/
-    if (!read_elf_header(file)) {
-        print_error("Not an ELF file");
-    }
-    /* Read the rest of the header*/
-    bytes_read = fread(&header, sizeof(header), 1, file);
-    if (bytes_read != 1) {
-        print_error("Failed to read ELF header");
-    }
-    /*Print the header information*/
-    printf("Magic: %c%c%c%c\n", header.magic[0], header.magic[1], header.magic[2], header.magic[3]);
-    printf("Class: %u\n", header.class);
-    printf("Data: %u\n", header.data);
-    printf("Version: %u\n", header.version);
-    printf("OS/ABI: %u\n", header.osabi);
-    printf("ABI Version: %u\n", header.abiversion);
-    printf("Type: %u\n", header.type);
-    printf("Entry Point Address: 0x%lx\n", header.entrypoint);
-    fclose(file);
+    display_elf_header(argv[1]);
+
     return 0;
 }
