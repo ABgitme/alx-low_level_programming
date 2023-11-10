e0840eec8fb499686c75016ceeef4a4dd1953f30
ABgitme <abebekezhabesha@gmail.com>
2023-11-10 18:21:42 +0300

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * dlistint_len - returns the number of elements in a d.linked list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
+ **/
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
