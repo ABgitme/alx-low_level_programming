33f51bc2043d6d0afb353297ae589b7a7eccc7d5
ABgitme <abebekezhabesha@gmail.com>
2023-11-13 16:54:19 +0300

    update 100

---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,12 @@
+#ifndef _100_OPERATIONS_H_
+#define _100_OPERATIONS_H_
+
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+#endif // _100_OPERATIONS_H_
+
