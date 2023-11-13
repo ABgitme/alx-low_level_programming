e24418823ef424c79fb7b5d4f20304eeb7c21202
ABgitme <abebekezhabesha@gmail.com>
2023-11-13 17:45:15 +0300

    update 100

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,21 +1,56 @@
 #include "100-operations.h"
 
+
+/**
+ * add - a function that adds two int values
+ * @a: value 1 
+ * @b: value 2
+ *
+ * Return: added value
+ */
 int add(int a, int b) {
   return a + b;
 }
 
+/**
+ * sub - a function that subtracts two int values
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: subtracted value
+ */
 int sub(int a, int b) {
   return a - b;
 }
 
+/**
+ * mul - a function that multiply two int values
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: multiplied  value
+ */
 int mul(int a, int b) {
 	  return a * b;
 }
 
+/**
+ * div - a function that divide  two int values
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: divided  value
+ */
 int div(int a, int b) {
   return a / b;
 }
-
+/**
+ * mod - a function that mod two int values
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: mod value
+ */
 int mod(int a, int b) {
   return a % b;
 }
