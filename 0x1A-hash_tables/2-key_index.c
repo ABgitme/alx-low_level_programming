5086fdfe37298b4bcf422a3fc738a8c09409b59b
ABgitme <abebekezhabesha@gmail.com>
2023-12-03 18:21:30 +0200

    3-hash_table_set

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -14,17 +14,4 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
     return (hash % size);
 }
 
-/**
- * key_index2 - function that returns the index of a key
- * @key: the key to search for the index
- * @size: size of the hash table
- * Return: index of the key
- */
-unsigned long int key_index2(const char *key, unsigned long int size)
-{
-    unsigned long int hash;
-
-    hash = hash_djb2_2(key);
-    return (hash % size);
-}
 
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,12 +4,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int index;
     hash_node_t *current_node, *new_node;
+    char *value_copy = NULL;
 
-    if (!key || !*key) {
-        return 0;
-    }
+    if (!ht || !key || !*key || !value)
+		return (0);
 
-    index = key_index2(key, ht->size);
+    index = key_index((const unsigned char *)key, ht->size);
 
     /*Check for collision*/
     current_node = ht->array[index];
@@ -17,7 +17,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         if (strcmp(current_node->key, key) == 0) {
             /*Collision detected, update the value*/
             free(current_node->value);
-            current_node->value = strdup(value);
+            value_copy = strdup(value);
+            if (!value_copy)
+                return 0;
+            current_node->value = value_copy;
             return 1;
         }
 
@@ -29,12 +32,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     /*Allocate memory for the new node*/
     new_node = malloc(sizeof(hash_node_t));
     if (!new_node) {
+        free(value_copy);
         return 0;
     }
 
     /*Copy the key and value to the new node*/
     new_node->key = strdup(key);
+    if (!new_node->key)
+    {
+        free(new_node);
+        return (0);
+    }
     new_node->value = strdup(value);
+    if (!new_node->value)
+    {
+        free(new_node);
+        free(new_node->key);
+        return (0);
+    }
     new_node->next = NULL;
 
     /*Add the new node to the beginning of the linked list*/
