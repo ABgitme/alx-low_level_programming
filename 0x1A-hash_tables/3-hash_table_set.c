0de9bf6ae9825dbc30081df2088ef7f137dcf8cf
ABgitme <abebekezhabesha@gmail.com>
2023-12-07 01:07:22 +0200

    4-hash_table_get

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,60 +1,64 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: on failure - 0. Otherwise - 1.
+ *
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *current_node, *new_node;
-    char *value_copy = NULL;
+	unsigned long int index;
+	hash_node_t *current_node, *new_node;
+	char *value_copy = NULL;
 
-    if (!ht || !key || !*key || !value)
+	if (!ht || !key || !*key || !value)
 		return (0);
-
-    index = key_index((const unsigned char *)key, ht->size);
-
-    /*Check for collision*/
-    current_node = ht->array[index];
-    while (current_node) {
-        if (strcmp(current_node->key, key) == 0) {
-            /*Collision detected, update the value*/
-            free(current_node->value);
-            value_copy = strdup(value);
-            if (!value_copy)
-                return 0;
-            current_node->value = value_copy;
-            return 1;
-        }
-
-        current_node = current_node->next;
-    }
-
-    /*No collision, continue with regular insertion*/
-
-    /*Allocate memory for the new node*/
-    new_node = malloc(sizeof(hash_node_t));
-    if (!new_node) {
-        free(value_copy);
-        return 0;
-    }
-
-    /*Copy the key and value to the new node*/
-    new_node->key = strdup(key);
-    if (!new_node->key)
-    {
-        free(new_node);
-        return (0);
-    }
-    new_node->value = strdup(value);
-    if (!new_node->value)
-    {
-        free(new_node);
-        free(new_node->key);
-        return (0);
-    }
-    new_node->next = NULL;
-
-    /*Add the new node to the beginning of the linked list*/
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-
-    return 1;
+	index = key_index((const unsigned char *)key, ht->size);
+	/*Check for collision*/
+	current_node = ht->array[index];
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			/*Collision detected, update the value*/
+			free(current_node->value);
+			value_copy = strdup(value);
+			if (!value_copy)
+				return (0);
+			current_node->value = value_copy;
+			return (1);
+		}
+		current_node = current_node->next;
+	}
+	/*No collision, continue with regular insertion*/
+	/*Allocate memory for the new node*/
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		free(value_copy);
+		return (0);
+	}
+	/*Copy the key and value to the new node*/
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node);
+		free(new_node->key);
+		return (0);
+	}
+	new_node->next = NULL;
+	/*Add the new node to the beginning of the linked list*/
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,19 +1,28 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - Retrieve the value associated with
+ *                  a key in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
+ *
+ * Return: no key matched - NULL.
+ *         Otherwise - value associated with key in ht.
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *node;
+	unsigned long int index;
+	hash_node_t *node;
 
-    if (!ht || !key || !*key)
+	if (!ht || !key || !*key)
 		return (0);
-    index = key_index((const unsigned char *)key, ht->size);
-    node = ht->array[index];
-    while (node)
-    {
-        if (strcmp(node->key, key) == 0)
-            return (node->value);
-    node = node->next;
-    }
-    return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
 }
